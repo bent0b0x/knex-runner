12d0a67733016202001945ebaa537700ae59e677
Benjamin Baum <bjb253@cornell.edu>
2016-04-11 12:45:19 -0500

    (feat) optional logging

--- knex-runner.js
+++ knex-runner.js
@@ -3,36 +3,43 @@ var path = require('path');
 
 var knex;
 
-var runner = function (knexOptions, schema, callback) {
+var runner = function (knexOptions, schema, callback, log) {
+
   callback = callback || function () {};
+
+  if (log === undefined) {
+    log = true;
+  }
+
   knex = knex || Knex(knexOptions);
+  
   if (schema.length) {
     var command = schema.pop();
     if (typeof command === 'function') {
       command()
       .then(function () {
-        runner(knexOptions, schema);
+        runner(knexOptions, schema, callback, log);
       })
       .catch(function (error) {
-        console.log('Knex Error: ',error);
+        log && console.log('Knex Error: ',error);
         callback(error);
       });
     } else {
-      runner(knexOptions, schema);
+      runner(knexOptions, schema, callback, log);
     }
   } else {
-    console.log('Knex Schema Built');
+    log && console.log('Knex Schema Built');
     if (!knexOptions.migrations) {
       callback();
       return;
     }
     knex.migrate.latest()
     .then(function () {
-      console.log('Knex Migrations Complete');
+      log && console.log('Knex Migrations Complete');
       callback();
     })
     .catch(function (error) {
-      console.error('Knex Migration Error: ', error);
+      log && console.error('Knex Migration Error: ', error);
       callback(error);
     });
   }
